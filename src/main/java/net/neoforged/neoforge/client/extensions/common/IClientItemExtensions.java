0a915f83e702710e5471e22ec0b48e1db8ba7d08
TheEnderCore <theendercore@gmail.com>
2026-03-24 12:20:49 +0200

    alpha 2

--- src/main/java/net/neoforged/neoforge/client/extensions/common/IClientItemExtensions.java
+++ src/main/java/net/neoforged/neoforge/client/extensions/common/IClientItemExtensions.java
@@ -3,7 +3,10 @@ package net.neoforged.neoforge.client.extensions.common;
 import com.github.alexthe666.citadel.mixin.refabricated.ItemRendererAccessor;
 import com.github.alexthe666.citadel.refabrciated.client.ClientExtensionsManager;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
@@ -21,5 +24,9 @@ public interface IClientItemExtensions {
 
     default BlockEntityWithoutLevelRenderer getCustomRenderer() {
         return ((ItemRendererAccessor) Minecraft.getInstance().getItemRenderer()).citadel_getBlockEntityRenderer();
+    }
+
+    default HumanoidModel<?> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, HumanoidModel<?> original) {
+        return original;
     }
 }
